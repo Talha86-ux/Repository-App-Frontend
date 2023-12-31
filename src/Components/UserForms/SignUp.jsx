47caade813528e5d135104edf1912d7ea08185b3
Talha86-ux <rjtalha555@gmail.com>
2023-12-31 19:20:50 +0500

    Handle user registration and login forms dynamically

--- src/Components/UserForms/SignUp.jsx
+++ src/Components/UserForms/SignUp.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './userForms.css'
 
 export const SignUp = () => {
+
+  const [action, setAction] = useState('Signup')
+
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>{action}</div>
       </div>
       <div className='inputs'>
+        {action === 'Login' ? <div></div> : <div className='input'>
+          <img src='' alt=''/>
+          <input type='text' placeholder='First Name' />
+        </div>}
+
+        {action === 'Login' ? <div></div> : <div className='input'>
+          <img src='' alt=''/>
+          <input type='text' placeholder='Last Name' />
+        </div>}
+
         <div className='input'>
           <img src='' alt=''/>
-          <input type='text' />
+          <input type='email' placeholder='Email' />
         </div>
 
         <div className='input'>
           <img src='' alt=''/>
-          <input type='email' />
+          <input type='password' placeholder='Password' />
         </div>
+      </div>
 
-        <div className='input'>
-          <img src='' alt=''/>
-          <input type='password' />
-        </div>
-      </div>
-      <div className='forgot-password'>Forgot Password? 
-        <span>Click Here!</span>
-      </div>
+      {action === 'Login' ? <div className='forgot-password'>Forgot Password? <span>Click Here!</span></div> : <div></div> }
+      
       <div className='submit-container'>
-        <div className='submit'>Register</div>
-        <div className='submit'>Login</div>
+        <div className= {action === 'Login' ? 'sumbit' : 'submit gray'} onClick = {()=> {setAction('Signup')}}>Sign Up</div>
+        <div className={action === 'Signup' ? 'sumbit' : 'submit gray'} onClick = {()=> {setAction('Login')}}>Login</div>
       </div>
     </div>
   )
